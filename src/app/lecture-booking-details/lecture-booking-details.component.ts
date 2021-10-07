a8f3326f9fe96a91f7b01e79c1046585168b67ef
kgoromolepo <kgoromolepo@outlook.com>
2021-10-07 15:53:30 +0200

    lectureBookings,profile,booking-detailsBykgoro

--- src/app/lecture-booking-details/lecture-booking-details.component.ts
+++ src/app/lecture-booking-details/lecture-booking-details.component.ts
@@ -1,15 +1,158 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { Router, NavigationExtras} from '@angular/router';
+import { HttpClient } from '@angular/common/http'; 
+import Swal from 'sweetalert2';
+
+
+//Exports class for bookings to store database data
+export class bookings {
+  constructor(
+    public Booking_ID: string,
+    public Lab_Name: string,
+    public Lab_Slot: string,
+    public Num_Bookings: string,
+    public Stud_ID: string,
+    public date: string,
+   
+  ) {
+  }
+}
+//Exports class for details to store localstorage data
+export class Details{
+  constructor(
+    public stud_no: string,
+    public stu_name: string,
+    public stud_surname: string,
+    public email: string,
+  ){}
+}
 
 @Component({
   selector: 'app-lecture-booking-details',
   templateUrl: './lecture-booking-details.component.html',
   styleUrls: ['./lecture-booking-details.component.css']
 })
-export class LectureBookingDetailsComponent implements OnInit {
 
-  constructor() { }
+
+export class LectureBookingDetailsComponent  implements OnInit {
+
+  constructor(private http:HttpClient,private router: Router) { }
+  //variable to store the token
+  tittle: string;
+  //array to store the data from the database
+  booking:bookings[];
+ //array to store the data from the localstorage
+ detail: Details[];
+ //variable to store the student Number
+ stuNumber: number;
+ //variable that store the number of booking
+ Num_Bookings: string;
 
   ngOnInit(): void {
+    this.tittle = localStorage.getItem("token");
+    this.getDetails();
   }
+
+    //get Detail funtion that store the data from local storage to the detail array
+    //and connects to booking API
+    getDetails(){
+      this.detail = JSON.parse(this.tittle);
+        this.stuNumber = JSON.parse(this.detail[0].stud_no);
+        
+        var st = JSON.stringify(this.stuNumber)
+        console.log(st);
+
+        //converting string to object
+        var jsonPerson = '{"stuNumber":'+ st +'}';
+        var personObject = JSON.parse(jsonPerson);
+
+        //Retrieve information from the database
+      this.http.post('http://localhost:3000/bookingStatus',personObject,{responseType:'text'})
+      .subscribe((result) =>{
+        this.booking = JSON.parse(result);
+        console.warn("Results", result);
+        //this.booking = result;
+      });
+      //API for number of bookings
+      this.http.post('http://localhost:3000/bookingsNum',personObject,{responseType:'text'})
+      .subscribe((result) =>{
+        //this.Num_Bookings = JSON.stringify(result);
+        this.Num_Bookings = result;
+        console.log(this.Num_Bookings);
+      });
+      console.warn(jsonPerson);
+
+        
+    }
+
+    onDelete(data){    
+      
+
+      var jsonPerson = '{"stuNumber":'+ data +'}';
+      var personObject = JSON.parse(jsonPerson);
+  
+      console.log(data)
+      Swal.fire({
+        title: 'Are you sure you want to cancel this booking',
+        text: '',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'YES',
+        cancelButtonText: 'NO'
+      }).then((result) => {
+        if (result.isConfirmed) {
+    
+          //Add the User to the Database
+          this.http.post('http://localhost:3000/cancelBooking',personObject,{responseType: 'text'})
+          .subscribe((result)=>{
+              console.warn("result",result)
+              if(result == 'The student is successfully deleted')
+              {
+                Swal.fire(
+                  'user has been successfuly deleted',
+                  '',
+                  'success'
+                )
+              }else{
+                
+                Swal.fire(
+                  result,
+                  '',
+                  'warning'
+                )
+               
+              }
+              
+          })
+          console.warn(data);
+    
+          
+          
+        
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire(
+            'Cancelled!',
+            '',
+            'error'
+          )
+        }
+      })
+
+    }
+
+    //on submit function that calls the booking detail API
+    onSubmit(data){
+    
+    }
+
+
+    //On click function for logout
+      onClick()
+      {
+        
+        localStorage.removeItem("token");
+        this.router.navigate(['/index']);
+      }
 
 }
--- src/app/lecture-lab-bookings/lecture-lab-bookings.component.ts
+++ src/app/lecture-lab-bookings/lecture-lab-bookings.component.ts
@@ -1,4 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder,Validators } from '@angular/forms';
+import { IssueService } from '../issue.service';
+import { Router} from '@angular/router';
+import { HttpClient } from '@angular/common/http'; 
+import Swal from 'sweetalert2';
+import { GroupedObservable } from 'rxjs';
+import { __values } from 'tslib';
+import { FormsModule } from '@angular/forms';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+
+////////******************************************************************************
+
+
+//storing info receiced from the console
+//
+export class Labs {
+  constructor(
+    public Lab_Name: string,
+    public Lab_Slot: string,
+    public time: string,
+    public descr: string,
+
+   
+  ) {
+  }
+}
+//export class for students for storing data from localstorage
+export class students {
+  constructor(
+    public stud_no: string,
+    public stu_name: string,
+    public stud_surname: string,
+    public email: string,
+    
+  ) {
+  }
+}
+
 
 @Component({
   selector: 'app-lecture-lab-bookings',
@@ -6,10 +44,185 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lecture-lab-bookings.component.css']
 })
 export class LectureLabBookingsComponent implements OnInit {
+  /*
+  reloadCurrentRoute() {
+    let currentUrl = this._router.url;
+    this._router.navigateByUrl('/lab-booking', {skipLocationChange: true}).then(() => {
+        this._router.navigate([currentUrl]);
+        console.log(currentUrl);
+    });
+  }*/
+  //constructor
+  constructor(private http:HttpClient, private router: Router, private fb:FormBuilder) { }
+  //lab array that store the labs from the database
+  lab: Labs[];
+  //variable that holds a token
+  tittle:string
+  //students array that holds data retrieved from localstorage
+  student:students[];
+  //variable that holds student Number
+  studentNo;
 
-  constructor() { }
+  //disable form input fields
+  disableSelect = true;
 
+  selected:string = ""
+
+
+  //variable to store the selected radio button
+  book = 'book';
+  cancel = 'cancel';
+  //On initialize function
   ngOnInit(): void {
+    this.getlab();
+    this.tittle = localStorage.getItem("token")
+    this.getStudents();
+   this.selected
+  }
+
+
+  //get function that receive the results from the database
+  getlab(){
+    this.http.get<any>('http://localhost:3000/availableLabs').subscribe(
+      response => {
+       
+        
+        this.lab = response;
+     
+        console.log(response);
+      }
+    );
+  }
+
+
+
+  //get the student number from the database
+  getStudents(){
+
+    this.student = JSON.parse(this.tittle);
+    this.studentNo = this.student[0].stud_no;
+    console.log(this.studentNo);
+  }
+
+
+  //onclick function that cancel booking
+ /* onClickCancel(data){
+    //sweet Alerts pop up messages
+    Swal.fire({
+      title: 'Cancel this booking?',
+      text: '',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'YES',
+      cancelButtonText: 'NO'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // TODO: Use EventEmitter with form value
+        //console.warn(this.ngForm.value);
+        
+
+        //Add the User to the Database
+        this.http.post('http://localhost:3000/cancelBooking',data, {responseType:'text'})
+        .subscribe((result)=>{
+            console.warn("result",result)
+            //On submit validation
+            if(result == 'booking has been cancelled')
+            {
+              Swal.fire(
+                result,
+                '',
+                'success'
+              )
+              //Navigate to the Login page
+              
+            }else{
+              Swal.fire(
+                result,
+                '',
+                'warning'
+              )
+            }
+
+            
+        })
+        console.warn(data);
+        
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Could Not cancel booking',
+          'error'
+        )
+      }
+    })
+  }*/
+ 
+  //On submit button
+  onSubmit(data)
+  {
+    
+        //sweet Alerts pop up messages
+    Swal.fire({
+      title: 'Book A Lab?',
+      text: '',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'YES',
+      cancelButtonText: 'NO'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // TODO: Use EventEmitter with form value
+        //console.warn(this.ngForm.value);
+        
+
+        //Add the User to the Database
+        this.http.post('http://localhost:3000/book',data, {responseType:'text'})
+        .subscribe((result)=>{
+            console.warn("result",result)
+            //On submit validation
+            if(result == 'you have successfully booked for a lab')
+            {
+              Swal.fire(
+                result,
+                '',
+                'success'
+              )
+              //Navigate to the Login page
+              
+            }else{
+              Swal.fire(
+                result,
+                '',
+                'warning'
+              )
+            }
+
+            
+        })
+        console.warn(data);
+        
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Could Not Add The User!',
+          'error'
+        )
+      }
+    })
+
+  }
+
+
+  refresh(): void {
+    window.location.reload();
+  }
+
+  //On click function for logout
+  onClick()
+  {
+    
+    localStorage.removeItem("token");
+    this.router.navigate(['/index']);
   }
 
 }
